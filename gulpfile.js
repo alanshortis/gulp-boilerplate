26d081242fc67b5f74a5b3a9c10b4034c095ee62
Alan Shortis <alan.shortis@mac.com>
2016-01-31 19:36:34 +0000

    Added header to minified CSS

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,17 @@ var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
 var rename = require('gulp-rename');
+var header = require('gulp-header');
 
 
-var sassSrc = 'sass/**/*.scss',
+var sassSrc = 'sass/**/*.scss';
 var cssDest = 'css';
 
 gulp.task('css', function () {
     return gulp.src(sassSrc)
         .pipe(sourcemaps.init())
-        .pipe(sass(outputStyle: 'expanded').on('error', sass.logError))
-        .pipe(autoprefixer(browsers: ['last 2 versions']))
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer({browsers: ['last 2 versions']}))
         .pipe(sourcemaps.write())
         .pipe(gulp.dest(cssDest));
 });
@@ -25,6 +26,7 @@ gulp.task('minify', ['css'], function() {
     return gulp.src(cssDest + '/style.css')
         .pipe(rename('style.min.css'))
         .pipe(cssnano())
+        .pipe(header('/* <%= new Date() %> */\n'))
         .pipe(gulp.dest(cssDest));
 });
 
