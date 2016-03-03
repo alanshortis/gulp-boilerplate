d626d7d395d6844d1e768ec510720fae1ec19d6e
Alan Shortis <alan.shortis@mac.com>
2016-03-03 10:23:06 +0000

    Closes #14: Added plugin loader

--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,14 @@
 'use strict';
 
-// Get our package manifest so we can use it's content
-const pkg = require('./package.json');
-
-// Import packages
+// Import packages and out package.json so we can use its content.
 const gulp = require('gulp'),
-      autoprefixer = require('gulp-autoprefixer'),
-      cheerio = require('gulp-cheerio'),
-      concat = require('gulp-concat'),
-      cssnano = require('gulp-cssnano'),
-      eslint = require('gulp-eslint'),
-      header = require('gulp-header'),
-      image = require('gulp-image'),
-      modernizr = require('gulp-modernizr'),
-      rename = require('gulp-rename'),
-      sass = require('gulp-sass'),
-      sourcemaps = require('gulp-sourcemaps'),
-      stripDebug = require('gulp-strip-debug'),
-      svgmin = require('gulp-svgmin'),
-      svgstore = require('gulp-svgstore'),
-      uglify = require('gulp-uglify'),
       del = require('del'),
       merge = require('merge-stream'),
-      notifier = require('node-notifier');
+      notifier = require('node-notifier'),
+      plugins = require('gulp-load-plugins')(),
+      pkg = require('./package.json');
 
-// Source folders
+// Source and Destination folders
 const src = {
   src: 'src',
   sass: 'src/sass/**/*.scss',
@@ -33,8 +17,6 @@ const src = {
   js: 'src/js',
   vendor: 'src/js/vendor'
 };
-
-// Destination folders
 const dest = {
   css: 'dist/css',
   img: 'dist/img',
@@ -48,8 +30,8 @@ const fileHeader = `/* ${pkg.name} | ${new Date()} */\n`;
 // Build CSS from SASS with sourcemaps and autoprefix.
 gulp.task('css', () => {
   return gulp.src(src.sass)
-    .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'expanded'})
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({outputStyle: 'expanded'})
     .on('error', function(err) {
       sass.logError.call(this, err);
       notifier.notify({
@@ -57,10 +39,10 @@ gulp.task('css', () => {
         message: 'SASS error - see terminal for details.'
       });
     }))
-    .pipe(autoprefixer({
+    .pipe(plugins.autoprefixer({
       browsers: ['last 2 versions']
     }))
-    .pipe(sourcemaps.write())
+    .pipe(plugins.sourcemaps.write())
     .pipe(gulp.dest(dest.css));
 });
 
@@ -68,11 +50,11 @@ gulp.task('css', () => {
 // Minify CSS.
 gulp.task('minify', ['css'], () => {
   return gulp.src(`${dest.css}/style.css`)
-    .pipe(rename({
+    .pipe(plugins.rename({
       suffix: '.min'
     }))
-    .pipe(cssnano())
-    .pipe(header(fileHeader))
+    .pipe(plugins.cssnano())
+    .pipe(plugins.header(fileHeader))
     .pipe(gulp.dest(dest.css));
 });
 
@@ -80,7 +62,7 @@ gulp.task('minify', ['css'], () => {
 // Custom modernizr build
 gulp.task('modernizr', () => {
   gulp.src([`${src.src}/**/*.{scss,js}`, `!${src.vendor}/**/*.js`])
-    .pipe(modernizr({
+    .pipe(plugins.modernizr({
       'cache': true,
       'options': [
         'mq',
@@ -94,7 +76,7 @@ gulp.task('modernizr', () => {
 // Concatenate vendor scripts and copy project code to the dist folder.
 gulp.task('js', ['modernizr'], () => {
   const vendorScripts = gulp.src(`${src.vendor}/*.js`)
-    .pipe(concat('libs.js'))
+    .pipe(plugins.concat('libs.js'))
     .pipe(gulp.dest(dest.js));
 
   const appScripts = gulp.src(`${src.js}/project.js`)
@@ -107,9 +89,9 @@ gulp.task('js', ['modernizr'], () => {
 // Lint our JavaScript, aside from node modules and vendor scripts. Rule are in .eslintrc.
 gulp.task('eslint', ['js'], () => {
   return gulp.src(['gulpfile.js', `${src.js}/*.js`])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError())
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError())
     .on('error', () => {
       notifier.notify({
         title: 'Gulp',
@@ -122,12 +104,12 @@ gulp.task('eslint', ['js'], () => {
 // Remove debug stuff (console.log, alert), uglify and save with a .min suffix.
 gulp.task('uglify', ['eslint'], () => {
   return gulp.src([`${dest.js}/*.js`, `!${dest.js}/**/*.min.js`])
-    .pipe(rename({
+    .pipe(plugins.rename({
       suffix: '.min'
     }))
-    .pipe(stripDebug())
-    .pipe(uglify())
-    .pipe(header(fileHeader))
+    .pipe(plugins.stripDebug())
+    .pipe(plugins.uglify())
+    .pipe(plugins.header(fileHeader))
     .pipe(gulp.dest(dest.js));
 });
 
@@ -135,7 +117,7 @@ gulp.task('uglify', ['eslint'], () => {
 // Optimise images and put them in the dist folder.
 gulp.task('images', () => {
   return gulp.src([`${src.img}/*`, `!${src.img}/icons`])
-    .pipe(image())
+    .pipe(plugins.image())
     .pipe(gulp.dest(dest.img));
 });
 
@@ -143,18 +125,18 @@ gulp.task('images', () => {
 // Make an SVG sprite.
 gulp.task('svgsprite', () => {
   return gulp.src(src.icons)
-  .pipe(svgmin())
-  .pipe(cheerio({
+  .pipe(plugins.svgmin())
+  .pipe(plugins.cheerio({
     run: function ($) {
       $('[fill]').removeAttr('fill');
       $('style').remove();
     },
     parserOptions: { xmlMode: true }
   }))
-  .pipe(rename({
+  .pipe(plugins.rename({
     prefix: 'icon-'
   }))
-  .pipe(svgstore())
+  .pipe(plugins.svgstore())
   .pipe(gulp.dest(dest.img));
 });
 
